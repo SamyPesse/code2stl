9f9cf46870902022721be01c4d8d8c9a573134b7
Samy Pessé <samypesse@gmail.com>
2014-04-22 23:28:27 +0200

    Add write of text

--- test.js
+++ test.js
@@ -2,15 +2,19 @@ var fs = require("fs");
 var stl = require('stl');
 
 var bitmapToStl = require("./lib/bitmap").toStl;
+var textToBitmap = require("./lib/text").textToBitmap;
 
-var image = [
+
+var image = textToBitmap("Hello");
+
+/*[
   [1, 1, 0, 0, 0, 1],
   [0, 1, 1, 1, 0, 1],
   [0, 1, 0, 1, 0, 1],
   [0, 1, 1, 1, 1, 1],
   [1, 1, 1, 1, 1, 1],
   [1, 1, 1, 0, 0, 1]
-];
+];*/
 
 var boxes = bitmapToStl(image);
 
